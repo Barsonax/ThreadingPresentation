ca1e45550fdc44915846b4c47e0e0332c09268f1
Rick van Dam <damric@uptrends.com>
2021-05-11 16:31:11 +0200

    CompareAndSwapExample example

--- build/Build.cs
+++ build/Build.cs
@@ -114,6 +114,7 @@ class Build : NukeBuild
                 // 1. Read counter, we get a value of 3                
                 // 2. Add 1 to counter to get 4
                 // 3. Store 4 in counter
+                
                 // However with multiple threads this can happen:
                 // 1. Read counter, we get a value of 3                
                 // 2. Add 1 to counter to get 4
@@ -134,5 +135,42 @@ class Build : NukeBuild
                 Interlocked.Increment(ref counter);
             });
             Console.WriteLine(counter);
+        });
+
+    public class DataClass {
+        public DataClass(int someNumber, int someNegativeNumber)
+        {
+            SomeNumber = someNumber;
+            SomeNegativeNumber = someNegativeNumber;
+        }
+
+    public int SomeNumber { get; }
+    public int SomeNegativeNumber { get; }
+}
+
+    Target CompareAndSwapExample => _ => _
+        .Executes(() =>
+        {      
+            var totalValue = new DataClass(0, 0);
+            Parallel.For(0, 100, i =>
+            {
+                DataClass initialValue, newValue;
+                do
+                {   
+                    // First we are going to take a snapshot of our current state.
+                    initialValue = totalValue;
+
+                    // Lets do some work on our snapshot, without mutating anything shared to other threads.          
+                    newValue = new DataClass(initialValue.SomeNumber + 1, initialValue.SomeNegativeNumber - 1);
+                
+                // Check if the state was not modified in the meantime. 
+                // If not then we can safely update the state by swapping the reference else we have to repeat.
+                // Note: this is a simple example in which we just repeat all of the work if another thread modified the state
+                // However we can also do optimizations such as reusing the DataClass instance we already made.
+                } while (initialValue != Interlocked.CompareExchange(ref totalValue, newValue, initialValue));
+
+            });
+            Console.WriteLine(totalValue.SomeNumber);
+            Console.WriteLine(totalValue.SomeNegativeNumber);
         });
 }
