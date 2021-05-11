18a6e9250050f10d19e8eb3879d73d92fe6f6d7f
Rick van Dam <damric@uptrends.com>
2021-05-11 15:47:38 +0200

    wip

--- build/Build.cs
+++ build/Build.cs
@@ -38,14 +38,14 @@ class Build : NukeBuild
     Target ParallelForeach => _ => _
         .Executes(() =>
         {
-            Parallel.ForEach(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, i =>
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    Thread.Sleep(100);
-                    Console.WriteLine($"My thread is {Thread.CurrentThread.ManagedThreadId}");
-                }
-            });
+            Parallel.ForEach(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, i =>
+              {
+                  for (int j = 0; j < 10; j++)
+                  {
+                      Thread.Sleep(100);
+                      Console.WriteLine($"My thread is {Thread.CurrentThread.ManagedThreadId}");
+                  }
+              });
         });
 
     Target Locking => _ => _
@@ -53,16 +53,86 @@ class Build : NukeBuild
         {
             var locker = new object();
 
-            Parallel.ForEach(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, i =>
+            Parallel.ForEach(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, i =>
+              {
+                  lock (locker)
+                  {
+                      for (int j = 0; j < 10; j++)
+                      {
+                          Thread.Sleep(100);
+                          Console.WriteLine($"My thread is {Thread.CurrentThread.ManagedThreadId}");
+                      }
+                  }
+              });
+        });
+
+    Target DeadLock => _ => _
+    .Executes(() =>
+    {
+        var thread1 = new Thread(() =>
+        {
+            lock (typeof(int))
             {
-                lock (locker)
+                Console.WriteLine("Thread 1 got the int lock");
+                Thread.Sleep(1000);
+                lock (typeof(float))
                 {
-                    for (int j = 0; j < 10; j++)
-                    {
-                        Thread.Sleep(100);
-                        Console.WriteLine($"My thread is {Thread.CurrentThread.ManagedThreadId}");
-                    }
+                    Console.WriteLine("Thread 1 got both locks");
                 }
+
+            }
+        });
+
+        var thread2 = new Thread(() =>
+        {
+            lock (typeof(float))
+            {
+                Console.WriteLine("Thread 2 got the float lock");
+                Thread.Sleep(1000);
+                lock (typeof(int))
+                {                    
+                    Console.WriteLine("Thread 2 got both locks");
+                }
+            }
+        });
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+    });
+
+    Target ParallelIncrementWrong => _ => _
+        .Executes(() =>
+        {
+            var counter = 0;
+            Parallel.For(0, 100, i =>
+            {
+                // This is not threadsafe because the variable counter can change after reading it.
+                // In steps this normally happens:
+                // 1. Read counter, we get a value of 3                
+                // 2. Add 1 to counter to get 4
+                // 3. Store 4 in counter
+                // However with multiple threads this can happen:
+                // 1. Read counter, we get a value of 3                
+                // 2. Add 1 to counter to get 4
+                // 3. Other thread updates counter to 4
+                // 4. Store 4 in counter
+                // Now we ended up with the value of 4 while we should have 5!
+                counter = counter + 1;
             });
+            Console.WriteLine(counter);
+        });
+
+    Target ParallelIncrement => _ => _
+        .Executes(() =>
+        {
+            var counter = 0;
+            Parallel.For(0, 100, i =>
+            {
+                Interlocked.Increment(ref counter);
+            });
+            Console.WriteLine(counter);
         });
 }
